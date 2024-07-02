c0fc0ff820064b540948c4df5714ed6a2297077b
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-07-02 11:24:11 +0530

    Code clean-up

--- src/Components/News.js
+++ src/Components/News.js
@@ -9,6 +9,7 @@ const News = (props) => {
     const [loading, setLoading] = useState(false)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
+
     const updateNews = async () => {
         let url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=843746d56f8a4018a7f351d14bded79f&page=${page}&category=${category}`
         setLoading(true)
@@ -17,6 +18,7 @@ const News = (props) => {
         setLoading(false)
         setArticles(articles.concat(parsadData.articles));
     }
+
     const fetchMoreData = async () => {
         setPage(page + 1)
         let url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=843746d56f8a4018a7f351d14bded79f&page=${page}&category=${category}`
@@ -27,15 +29,19 @@ const News = (props) => {
         setArticles(articles.concat(parsadData.articles));
         setTotalResults(parsadData.totalResults);
     };
+
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
+
     document.title = ` ${capitalizeFirstLetter(
         props.category
     )} - NewsApp`;
+
     useEffect(() => {
         updateNews();
     }, []);
+
     return (
         <div className='container '>
             <h1 className='text-center' style={{ margin: "25px 0px", marginTop: "90px" }}>NewsApp - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
@@ -45,11 +51,9 @@ const News = (props) => {
                 hasMore={articles !== totalResults}
                 loader={loading && <Spinner />}
             >
-
                 <div className='row'>
                     {articles.map((element) => {
                         return (
-
                             <div className='col-md-4'>
                                 <NewsItems title={element.title}
                                     desc={element.description}
@@ -62,7 +66,6 @@ const News = (props) => {
                             </div>
                         )
                     })}
-
                 </div>
             </InfiniteScroll >
         </div>
